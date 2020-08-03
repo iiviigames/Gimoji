d8f3aa33cb78434992b9d93d2f8de6c201723276
Vii <iiviigames@pm.me>
2020-08-03 18:45:36 -0400

    :santa: Added the first stuff.

---
+++ emoji.bat
@@ -0,0 +1,8 @@
+@echo off 
+REM	Created by iivii
+REM	https://odd.codes
+REM	github.com/iiviigames/Gimoji
+REM	06/27/2020
+REM	Last updated 08/03/2020
+REM 	Runs the moji Python program if its stored on PATH, like the site-packages folder.
+py -3 -m moji %*
